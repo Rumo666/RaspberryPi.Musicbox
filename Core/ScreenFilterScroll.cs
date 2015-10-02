3959cf0445765da947fe32c13e2c6aee3ce02d90
mwinkler <mwinkler@gmx.ch>
2015-10-02 15:41:34 +0200

    Add test project

--- Core/ScreenFilterScroll.cs
+++ Core/ScreenFilterScroll.cs
@@ -9,6 +9,13 @@ namespace Jukebox.Core
     public class ScreenFilterScroll : IScreenFilter
     {
         private int _position;
+        private DateTime _lastPositionChange;
+        private bool _scrollBackward;
+
+        public ScreenFilterScroll(TimeSpan speed)
+        {
+            Speed = speed;
+        }
 
         public TimeSpan Speed { get; set; }
 
@@ -16,9 +23,35 @@ namespace Jukebox.Core
         {
             // need to scroll
             if (content.Length <= display.Columns)
-                return content;
+            {
+                // reset
+                _position = 0;
+                _scrollBackward = false;
 
-            return null;
+                return content;
+            }
+
+            // update position
+            if (_lastPositionChange.Add(Speed) < DateTime.Now)
+            {
+                _position = _position + (_scrollBackward ? -1 : 1);
+
+                // revert position
+                if (_position < 0)
+                {
+                    _position = 0;
+                    _scrollBackward = false;
+                }
+                else if (_position > content.Length - display.Columns)
+                {
+                    _position = content.Length - display.Columns;
+                    _scrollBackward = true;
+                }
+
+                _lastPositionChange = DateTime.Now;
+            }
+
+            return content.Substring(_position, display.Columns);
         }
     }
 }
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Common.Logging;
+using Jukebox.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private class MockDisplay : IDisplay
+        {
+            public int Columns { get; } = 16;
+            public int Rows { get; } = 2;
+        }
+
+        private static readonly ILog log = LogManager.GetLogger<UnitTest1>();
+
+        [TestMethod]
+        public void ScreenScrollFilter()
+        {
+            var end = DateTime.Now.Add(new TimeSpan(0, 0, 0, 30));
+            var filter = new ScreenFilterScroll(new TimeSpan(0, 0, 0, 0, 100));
+            var display = new MockDisplay();
+            var content = "Das ist ein langer Text der zum Testen dienen soll und nichts sinnvolles enthält";
+
+            while (true)
+            {
+                var output = filter.Render(content, display);
+
+                log.Debug($"[{output}]");
+
+                Thread.Sleep(100);
+
+                if (end < DateTime.Now)
+                    break;
+            }
+
+        }
+    }
+}
